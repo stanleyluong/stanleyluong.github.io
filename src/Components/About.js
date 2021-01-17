81bf01531f2322a8d5d4949b501bd8502b35de2a
Stanley Luong <stanleyluong@Stanleys-MacBook-Pro.local>
2021-01-16 22:49:20 -0800

    added info, photos

--- src/Components/About.js
+++ src/Components/About.js
@@ -28,19 +28,19 @@ class About extends Component {
             <p>{bio}</p>
             <div className="row">
                <div className="columns contact-details">
-                  <h2>Contact Details</h2>
+                  <h2>Contact Email</h2>
                   <p className="address">
-						   <span>{name}</span><br />
+						   {/* <span>{name}</span><br />
 						   <span>{street}<br />
-						         {city} {state}, {zip}
+						         {city} {state} {zip}
                    </span><br />
-						   <span>{phone}</span><br />
+						   <span>{phone}</span><br /> */}
                      <span>{email}</span>
 					   </p>
                </div>
                <div className="columns download">
                   <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
+                     {/* <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a> */}
                   </p>
                </div>
             </div>
