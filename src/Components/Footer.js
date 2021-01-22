0dfe895863311d9e780e878409c1c3ae7d0134ca
Stanley Luong <xstanz@gmail.com>
2021-01-21 19:33:07 -0800

    changed names

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,25 +2,19 @@ import React, { Component } from 'react';
 
 class Footer extends Component {
   render() {
-
     if(this.props.data){
       var networks= this.props.data.social.map(function(network){
         return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
       })
     }
-
     return (
       <footer>
-
      <div className="row">
         <div className="twelve columns">
            <ul className="social-links">
               {networks}
            </ul>
-
            <ul className="copyright">
-              {/* <li>&copy; Copyright 2017 Tim Baker</li> */}
-              {/* <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li> */}
            </ul>
 
         </div>
@@ -32,10 +26,3 @@ class Footer extends Component {
 }
 
 export default Footer;
-
-
-      // {
-      //   "name":"google-plus",
-      //   "url":"http://googleplus.com/tbakerx",
-      //   "className":"fa fa-google-plus"
-      // },
