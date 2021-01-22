5319aa4630a2219ef8e99e6e1c7d06d05cf3a5fe
Stanley Luong <xstanz@gmail.com>
2021-01-21 20:42:13 -0800

    description

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
 
     if(this.props.data){
       var name = this.props.data.name;
-      var occupation= this.props.data.occupation;
+      var occupation= this.props.data.occupation
       // var description= this.props.data.description;
       // var city= this.props.data.address.city;
       var networks= this.props.data.social.map(function(network){
@@ -15,12 +15,9 @@ class Header extends Component {
 
     return (
       <header id="home">
-
       <nav id="nav-wrap">
-
          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
 	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
-
          <ul id="nav" className="nav">
             <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
             <li><a className="smoothscroll" href="#about">About</a></li>
@@ -29,14 +26,12 @@ class Header extends Component {
             <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
             <li><a className="smoothscroll" href="#contact">Contact</a></li>
          </ul>
-
       </nav>
-
       <div className="row banner">
          <div className="banner-text">
             <h1 className="responsive-headline">{name}</h1>
-            <h3>a Seattle based <span>{occupation}</span>.</h3>
-            <hr />
+            <h3><span>{occupation}</span>.</h3>
+            <hr/>
             <ul className="social">
                {networks}
             </ul>
