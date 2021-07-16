e101372cf9f0c2c610b43784c7270b68c8aaf18c
Stanley Luong <slluong@uw.edu>
2021-07-15 18:50:03 -0700

    6/15/21 6:49pm

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -38,7 +38,7 @@ class Resume extends Component {
     return (
     <section id="resume">
       <div className="row work">
-        <div className="three columns header-col"><h1><span>Work</span></h1></div>
+        <div className="three columns header-col"><h1><span>Employment</span></h1></div>
         <div className="nine columns main-col">{work}</div>
       </div>
 
