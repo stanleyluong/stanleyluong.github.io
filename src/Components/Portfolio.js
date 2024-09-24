da4ecc5caf8cc078aeaf779e1bfb12e9115e9e08
Stanley Luong <stanleyluong@Stanleys-MacBook-Pro.local>
2024-09-24 15:27:33 -0700

    added meta projects to portfolio

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -4,22 +4,22 @@ class Portfolio extends Component {
   render() {
 
     if(this.props.data){
-      var portfolio = this.props.data.projects.slice(0,24).map(project => {
-      // var portfolio = this.props.data.projects.map(project => {
+      // var portfolio = this.props.data.projects.slice(0,24).map(project => {
+      var portfolio = this.props.data.projects.map(project => {
 
         var projectImage = 'images/portfolio/'+project.image
         return (
           <div key={project.title} className="columns portfolio-item">
             <div className="item-wrap">
               <a target="_blank" rel="noreferrer" href={project.url} title={project.title}>
-                  <img width="100%" loading="lazy" src={projectImage} alt={project.title} type="image/webp"/>
+                <img width="100%" loading="lazy" src={projectImage} alt={project.title} type="image/webp"/>
                 <div className="overlay">
-                    <div className="portfolio-item-meta">
-                  <h5>{project.title}</h5>
-                      <p>{project.category}</p>
-                    </div>
+                  <div className="portfolio-item-meta">
+                    <h5>{project.title}</h5>
+                    <p>{project.category}</p>
                   </div>
-                <div className="link-icon"><i className="fa fa-link"></i></div>
+                </div>
+                {/* <div className="link-icon"><i className="fa fa-link"></i></div> */}
               </a>
             </div>
           </div>
